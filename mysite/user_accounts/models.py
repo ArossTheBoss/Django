d486a5ead659b03a5f6d21b802bb371331174da9
Alex Rossomakha <ar444@MacBook-Pro.local>
2016-06-09 22:24:12 -0500

    changed country to state and re-ran migrations

--- mysite/user_accounts/models.py
+++ mysite/user_accounts/models.py
@@ -7,5 +7,5 @@ class User(models.Model):
     last_name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
     city = models.CharField(max_length=60)
-    country = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
 
--- mysite/user_accounts/views.py
+++ mysite/user_accounts/views.py
@@ -3,21 +3,24 @@ from .models import User
 import json
 
 
+
 # Create your views here.
+
+
 
 def create_user_info(request):
 
     request_data = json.loads(request.body.decode('utf-8'))
 
-
     user_data = {"first_name": request_data['first_name'],
                  "last_name": request_data['last_name'],
                  "address": request_data['address'],
                  "city": request_data['city'],
-                 "country": request_data['country']}
+                 "state": request_data['state']}
 
-    user = User().save(user_data)
+
+
+    User(**user_data).save()
 
     return HttpResponse(request)
-
 
